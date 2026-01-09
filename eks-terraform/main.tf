c50d96a2d45fa0411ee2e44f4be872ec50df32fa
Houyem <houyem@example.com>
2026-01-09 01:45:12 +0100

    Fix main.tf: set correct subnet and security group names

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -1,100 +1,15 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-# ----------------------------
-# IAM Roles existants (LabRole)
-# ----------------------------
-data "aws_iam_role" "master" {
-  name = "LabRole"
-}
-
-data "aws_iam_role" "worker" {
-  name = "LabRole"
-}
-
-# ----------------------------
-# VPC et Subnets
-# ----------------------------
-data "aws_vpc" "main" {
-  tags = {
-    Name = "Lab-VPC"
-  }
-}
-
 data "aws_subnet" "subnet-1" {
-  vpc_id = data.aws_vpc.main.id
   filter {
     name   = "tag:Name"
-    values = ["REPLACE_WITH_YOUR_SUBNET1_NAME"]
+    values = ["my-subnet-1"]
   }
-}
-
-data "aws_subnet" "subnet-2" {
   vpc_id = data.aws_vpc.main.id
-  filter {
-    name   = "tag:Name"
-    values = ["REPLACE_WITH_YOUR_SUBNET2_NAME"]
-  }
 }
 
 data "aws_security_group" "selected" {
-  vpc_id = data.aws_vpc.main.id
   filter {
-    name   = "tag:Name"
-    values = ["REPLACE_WITH_YOUR_SECURITY_GROUP_NAME"]
+    name   = "group-name"
+    values = ["my-security-group"]
   }
-}
-
-# ----------------------------
-# EKS Cluster
-# ----------------------------
-resource "aws_eks_cluster" "eks" {
-  name     = "MelCluster"
-  role_arn = data.aws_iam_role.master.arn
-
-  vpc_config {
-    subnet_ids         = [data.aws_subnet.subnet-1.id, data.aws_subnet.subnet-2.id]
-    security_group_ids = [data.aws_security_group.selected.id]
-  }
-
-  tags = {
-    Name        = "MelCluster"
-    Environment = "dev"
-    Terraform   = "true"
-  }
-}
-
-# ----------------------------
-# EKS Node Group
-# ----------------------------
-resource "aws_eks_node_group" "node-grp" {
-  cluster_name    = aws_eks_cluster.eks.name
-  node_group_name = var.node_group_name
-  node_role_arn   = data.aws_iam_role.worker.arn
-  subnet_ids      = [data.aws_subnet.subnet-1.id, data.aws_subnet.subnet-2.id]
-  capacity_type   = "ON_DEMAND"
-  disk_size       = 20
-  instance_types  = ["t2.large"]
-
-  scaling_config {
-    desired_size = 3
-    max_size     = 10
-    min_size     = 2
-  }
-
-  tags = {
-    Name = "lab-eks-node-group"
-  }
-}
-
-# ----------------------------
-# OIDC Provider pour ServiceAccount IAM Roles
-# ----------------------------
-data "aws_eks_cluster" "eks_oidc" {
-  name = aws_eks_cluster.eks.name
-}
-
-data "tls_certificate" "oidc_thumbprint" {
-  url = data.aws_eks_cluster.eks_oidc.identity[0].oidc[0].issuer
+  vpc_id = data.aws_vpc.main.id
 }
